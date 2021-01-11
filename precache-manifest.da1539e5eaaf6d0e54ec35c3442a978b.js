ee9d10aad90fdbbe185028ef7cfc910e117ef270
ritikasaxena <rsaxena@brandeis.edu>
2021-01-11 03:00:42 +0000

    Deploying to gh-pages from  @ 90222446f9d439ab01ea35d0f65c41c35c01ecaf 🚀

---
+++ precache-manifest.da1539e5eaaf6d0e54ec35c3442a978b.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c21aed4550b88bbf99cc3858468c3363",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d39d5aa954cee2c05c7e",
+    "url": "/portfolio/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "a6eb6c75f851ce612455",
+    "url": "/portfolio/static/css/4.a922a401.chunk.css"
+  },
+  {
+    "revision": "8fe40e4c8b67f02cd62c",
+    "url": "/portfolio/static/css/main.d25040c8.chunk.css"
+  },
+  {
+    "revision": "ca78619966b67cf91251",
+    "url": "/portfolio/static/js/2.dd5d0f1f.chunk.js"
+  },
+  {
+    "revision": "d39d5aa954cee2c05c7e",
+    "url": "/portfolio/static/js/3.f9b34351.chunk.js"
+  },
+  {
+    "revision": "a6eb6c75f851ce612455",
+    "url": "/portfolio/static/js/4.aa3d8e4d.chunk.js"
+  },
+  {
+    "revision": "8fe40e4c8b67f02cd62c",
+    "url": "/portfolio/static/js/main.8f4221a2.chunk.js"
+  },
+  {
+    "revision": "bd35b9332d6bcbf5ee9a",
+    "url": "/portfolio/static/js/runtime-main.eef84454.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/portfolio/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "b00d193678f805fea196f29c0bb0372e",
+    "url": "/portfolio/static/media/brandeisLogo.b00d1936.png"
+  },
+  {
+    "revision": "41a7200cd52f35768e09bcb007e2924b",
+    "url": "/portfolio/static/media/bwhLogo.41a7200c.png"
+  },
+  {
+    "revision": "28296457a3b0fb62af80c90f25ef9ed9",
+    "url": "/portfolio/static/media/cocLogo.28296457.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/portfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "62dfa17d07233f0ff26136215d1dcf5e",
+    "url": "/portfolio/static/media/codestelLogo.62dfa17d.png"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/portfolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "7b1e30170dbae01b1d53bde27d772811",
+    "url": "/portfolio/static/media/contactMe.7b1e3017.svg"
+  },
+  {
+    "revision": "adaee002d7546a177f19a669ff15c3de",
+    "url": "/portfolio/static/media/coursignerLogo.adaee002.png"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/portfolio/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "c24ba79111a90758a3a472a906ab2901",
+    "url": "/portfolio/static/media/googleAssistant.c24ba791.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "9fee0dbb73fe64207699d416bf3a8f26",
+    "url": "/portfolio/static/media/greeting.9fee0dbb.svg"
+  },
+  {
+    "revision": "de4a858dc2cc1aee9b2ab55d374f4086",
+    "url": "/portfolio/static/media/gwcposterLogo.de4a858d.png"
+  },
+  {
+    "revision": "fcf7fb29b5296262772e4d932f2316a0",
+    "url": "/portfolio/static/media/ihubLogo.fcf7fb29.png"
+  },
+  {
+    "revision": "b05ae5b575a7e1125610944dc152c35f",
+    "url": "/portfolio/static/media/jsFramework.b05ae5b5.svg"
+  },
+  {
+    "revision": "c2ba4434c3cb16d0cffda344276137c1",
+    "url": "/portfolio/static/media/jtrackLogo.c2ba4434.png"
+  },
+  {
+    "revision": "eff5b54a9a53299ceb783ef1a2705a36",
+    "url": "/portfolio/static/media/libertyLogo.eff5b54a.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "69134f0d4fa25a40d45f650a66e87c74",
+    "url": "/portfolio/static/media/makerlabLogo.69134f0d.png"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/portfolio/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "8619e7d94082da574529cb7d3ba3e843",
+    "url": "/portfolio/static/media/posterLogo.8619e7d9.png"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/portfolio/static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "1a60e8a9e2c2d6a539c7692f27fe6790",
+    "url": "/portfolio/static/media/pwa.1a60e8a9.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/portfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/portfolio/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/portfolio/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "d915394f6b51ff85fcd6b84c7a0f62f1",
+    "url": "/portfolio/static/media/sheinspiresLogo.d915394f.png"
+  },
+  {
+    "revision": "6a3a797335677fc8cf4ff0d99bb4722e",
+    "url": "/portfolio/static/media/skill.6a3a7973.svg"
+  },
+  {
+    "revision": "f1a87954baf0ca5b61375441f7593d1f",
+    "url": "/portfolio/static/media/ssUproar.f1a87954.PNG"
+  },
+  {
+    "revision": "4dd2c387b61f313ce2c7a9e52c710820",
+    "url": "/portfolio/static/media/takeoutLogo.4dd2c387.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/portfolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "b48e86c9a956d3a3a32ca4a5ae05ddc7",
+    "url": "/portfolio/static/media/talksCardBackAlt.b48e86c9.svg"
+  },
+  {
+    "revision": "270f8f0f5ae71e72e30d7e6064a58a63",
+    "url": "/portfolio/static/media/technology.270f8f0f.svg"
+  },
+  {
+    "revision": "48aa1dc4db08f007c9bb837d1e6fa2c3",
+    "url": "/portfolio/static/media/undraw_new_ideas_jdea.48aa1dc4.svg"
+  },
+  {
+    "revision": "209f380686017090223f752ea86f5a0d",
+    "url": "/portfolio/static/media/workgridLogo.209f3806.png"
+  }
+]);
